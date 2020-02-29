34d38229c43afbf776e5abda5c5b3327fae134e6
sebas <sebastien.damsin@gmail.com>
2020-02-29 21:20:26 +0100

    Modif images cartes

--- JeuMilleBorne/JeuMilleBorne/GestionCartes.cs
+++ JeuMilleBorne/JeuMilleBorne/GestionCartes.cs
@@ -11,16 +11,14 @@ namespace JeuMilleBorne
     {
         public void CreerPaquet(ref List<Carte> paquet)
         {
-            int index = 1;
             for (Carte.NomsCartes nomCarte = Carte.NomsCartes.Prioritaire; nomCarte <= Carte.NomsCartes.AsVolant; nomCarte++)
             {
                 Carte carte = new Carte();
                 carte.Nom = carte.RetournerTexteNomsCartes(nomCarte);
                 carte.Type = carte.RetournerTexteTypesCartes(Carte.TypesCartes.Botte);
-                carte.ImageCarte = (Image)Ressource.ResourceManager.GetObject(string.Format("mb-{0}", index));
+                carte.ImageCarte = (Image)Ressource.ResourceManager.GetObject(string.Format("mb_{0}", nomCarte));
+                //carte.ImageCarte = Ressource.;
                 paquet.Add(carte);
-
-                index++;
             }
         }
     }
