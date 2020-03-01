229d9d8be7745a7725480e599dac651c37b2e959
sebas <sebastien.damsin@gmail.com>
2020-03-01 01:02:46 +0100

    + Avancée sur la création du paquet de cartes + Simplification création d'une carte

--- JeuMilleBorne/JeuMilleBorne/GestionCartes.cs
+++ JeuMilleBorne/JeuMilleBorne/GestionCartes.cs
@@ -14,13 +14,47 @@ namespace JeuMilleBorne
             int index = 1;
             for (Carte.NomsCartes nomCarte = Carte.NomsCartes.Prioritaire; nomCarte <= Carte.NomsCartes.AsVolant; nomCarte++)
             {
-                Carte carte = new Carte();
-                carte.Nom = carte.RetournerTexteNomsCartes(nomCarte);
-                carte.Type = carte.RetournerTexteTypesCartes(Carte.TypesCartes.Botte);
-                carte.ImageCarte = (Image)Resource1.ResourceManager.GetObject(string.Format("mb_{0}", index));
-                paquet.Add(carte);
+                paquet.Add(CreerCarte(nomCarte, Carte.TypesCartes.Botte, index));
                 index++;
             }
+            for (int i = 0; i < 3; i++)
+            {
+                paquet.Add(CreerCarte(Carte.NomsCartes.Accident, Carte.TypesCartes.Attaque, index));
+            }
+            index++;
+            for (int i = 0; i < 3; i++)
+            {
+                paquet.Add(CreerCarte(Carte.NomsCartes.Creve, Carte.TypesCartes.Attaque, index));
+            }
+            index++;
+            for (int i = 0; i < 4; i++)
+            {
+                paquet.Add(CreerCarte(Carte.NomsCartes.Lim, Carte.TypesCartes.Attaque, index));
+            }
+            index++;
+            for (int i = 0; i < 3; i++)
+            {
+                paquet.Add(CreerCarte(Carte.NomsCartes.Panne, Carte.TypesCartes.Attaque, index));
+            }
+            index++;
+            for (int i = 0; i < 5; i++)
+            {
+                paquet.Add(CreerCarte(Carte.NomsCartes.Stop, Carte.TypesCartes.Attaque, index));
+            }
+            index++;
+            for (int i = 0; i < 3; i++)
+            {
+                paquet.Add(CreerCarte(Carte.NomsCartes.Stop, Carte.TypesCartes.Attaque, index));
+            }
+        }
+
+        public Carte CreerCarte(Carte.NomsCartes nomcarte, Carte.TypesCartes typecarte, int index)
+        {
+            Carte carte = new Carte();
+            carte.Nom = carte.RetournerTexteNomsCartes(nomcarte);
+            carte.Type = carte.RetournerTexteTypesCartes(typecarte);
+            carte.ImageCarte = (Image)Resource1.ResourceManager.GetObject(string.Format("mb_{0}", index));
+            return carte;
         }
     }
 }
